64de757637caaa4e062ff40097fe196a10c28593
rafalsky94 <rafalbatorowicz94@gmail.com>
2017-09-30 16:24:06 +0200

    Done mouseclick TODO: Compression

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     
+const mouseClick = document.querySelectorAll(".key");
+
+mouseClick.forEach(click => click.addEventListener("click", (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.target.dataset.key}"]`);
+    audio.play();
+    const button = document.querySelector(`.key[data-key="${e.target.dataset.key}"`);
+    button.classList.add("active");
+}));
 
 window.addEventListener("keydown", (e) => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -8,31 +16,16 @@ window.addEventListener("keydown", (e) => {
     audio.play();
     const button = document.querySelector(`.key[data-key="${e.keyCode}"`);
     button.classList.add("active");
-
-
-
 })
+
 
 function removeTransition(e) {
     if(e.propertyName !== "transform") return;
     
     this.classList.remove("active");
-  }
-  const keys = document.querySelectorAll(".key");
-  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
 });
